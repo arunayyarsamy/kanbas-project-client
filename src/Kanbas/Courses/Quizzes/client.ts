0c46b2dbceff0a6c144c2a191e5233e78442ed1c
Arun Ayyarsamy <ayyarsamy.a@northeastern.edu>
2024-04-24 11:19:34 -0400

    Courses for users changes

--- src/Kanbas/Courses/Quizzes/client.ts
+++ src/Kanbas/Courses/Quizzes/client.ts
@@ -3,6 +3,8 @@ const API_BASE = process.env.REACT_APP_API_BASE;
 const COURSES_API = `${API_BASE}/api/courses`;
 const QUIZZES_API = `${API_BASE}/api/quizzes`;
 
+axios.defaults.withCredentials = true;
+
 export const deleteQuiz = async (quizId: any) => {
   const response = await axios.delete(`${QUIZZES_API}/${quizId}`);
   return response.data;
